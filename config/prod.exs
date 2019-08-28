e0e0a8294bc025f22c901a5504b5620652c716ec
Jesus Velasquez <jesusvelasquezoficial@gmail.com>
2019-08-27 20:30:23 -0400

    updated heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,18 +13,19 @@ config :p_server, PServerWeb.Endpoint,
   # url: [host: "0.0.0.0", port: 80], #DEPLOYMENT
   url: [scheme:"https", host:"mighty-escarpment-88517.herokuapp.com", port: 433],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  ##########################################3
+  ##########################################
   cache_static_manifest: "priv/static/cache_manifest.json"
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
 
 config :p_server, PServer.Repo,
-  show_sensitive_data_on_connection_error: true,
-  pool_size: 18,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true,
-  url: System.get_env("DATABASE_URL")
+  show_sensitive_data_on_connection_error: true
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key

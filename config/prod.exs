3ced9cbc3aeb5146fc7791c27f464bcfd3453c0c
Jesus Velasquez <jesusvelasquezoficial@gmail.com>
2019-08-28 18:47:15 -0400

    update

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :p_server, PServerWeb.Endpoint,
   # force_ssl: [rewrite_on: [:x_forwarded_proto]],
   force_ssl: [hsts: true],
   ##########################################
-  cache_static_manifest: "priv/static/cache_manifest.json",
+  # cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production

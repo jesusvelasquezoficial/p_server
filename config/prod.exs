c1aa79e4fc493a1a3cda7c869d4561600f69da3c
Jesus Velasquez <jesusvelasquezoficial@gmail.com>
2019-08-27 21:15:09 -0400

    Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :p_server, PServerWeb.Endpoint,
   # url: [host: "0.0.0.0", port: 80], #DEPLOYMENT
-  url: [scheme:"https", host:"mighty-escarpment-88517.herokuapp.com", port: 433],
+  url: [scheme: "https", host: "mighty-escarpment-88517.herokuapp.com", port: 433],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   ##########################################
   cache_static_manifest: "priv/static/cache_manifest.json"

38bcaf14b547c8b3bcab5f9603a06381c396d42b
Jesus Velasquez <jesusvelasquezoficial@gmail.com>
2019-08-27 22:20:06 -0400

    updated

--- lib/p_server_web/router.ex
+++ lib/p_server_web/router.ex
@@ -17,7 +17,7 @@ defmodule PServerWeb.Router do
   scope "/", PServerWeb do
     pipe_through :browser
 
-    # get "/", PageController, :index
+    get "/", PageController, :index
   end
 
   # Other scopes may use custom stacks.

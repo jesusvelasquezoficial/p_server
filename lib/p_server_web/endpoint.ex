f14b8ece5578b1a76e607f579f211db403718339
John Doe <jesusvelasquezoficial@gmail.com>
2019-08-30 12:56:46 -0400

    u

--- lib/p_server_web/endpoint.ex
+++ lib/p_server_web/endpoint.ex
@@ -44,7 +44,6 @@ defmodule PServerWeb.Endpoint do
 
   # ADD el enchufe de la nueva dependencia
   plug CORSPlug, origin: "*"
-  plug Plug.SSL, rewrite_on: [:x_forwarded_proto]
 
   plug PServerWeb.Router
 

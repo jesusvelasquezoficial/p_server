728851ce15e3a11f5534dde38a7ac48dc3af4584
John Doe <jesusvelasquezoficial@gmail.com>
2019-09-02 12:15:31 -0400

    update

--- config/prod.exs
+++ config/prod.exs
@@ -15,12 +15,7 @@ config :p_server, PServerWeb.Endpoint,
   # force_ssl: [rewrite_on: [:x_forwarded_proto]],
   # force_ssl: [hsts: true],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  check_origin: ["https://phoenixserver.ml",
-                "https://www.phoenixserver.ml",
-                "http://10.0.1.14:8081",
-                "http://localhost:8081",
-                "http://localhost",
-                "*"]
+  check_origin: ["*"]
   ##########################################
   # cache_static_manifest: "priv/static/cache_manifest.json",
 

eca61010e7014101715904464d280cb0f51210ae
John Doe <jesusvelasquezoficial@gmail.com>
2019-09-02 12:18:43 -0400

    u

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :p_server, PServerWeb.Endpoint,
   # force_ssl: [rewrite_on: [:x_forwarded_proto]],
   # force_ssl: [hsts: true],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  check_origin: ["*"]
+  check_origin: ["*/*"]
   ##########################################
   # cache_static_manifest: "priv/static/cache_manifest.json",
 

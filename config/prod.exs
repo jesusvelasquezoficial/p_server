f4c03fbca69a3329e934e4e3cee2b0b3ae3b8c54
John Doe <jesusvelasquezoficial@gmail.com>
2019-09-02 11:53:37 -0400

    HEROKU

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,8 @@ config :p_server, PServerWeb.Endpoint,
   url: [scheme: "https", host: "mighty-escarpment-88517.herokuapp.com", port: 443],
   # force_ssl: [rewrite_on: [:x_forwarded_proto]],
   # force_ssl: [hsts: true],
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  check_origin: ["https://phoenixserver.ml", "https://www.phoenixserver.ml"]
   ##########################################
   # cache_static_manifest: "priv/static/cache_manifest.json",
 

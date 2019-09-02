300d92f2f25b368a4a09e3e45e22c59b00991a98
John Doe <jesusvelasquezoficial@gmail.com>
2019-09-02 14:26:46 -0400

    update

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :p_server, PServerWeb.Endpoint,
-  # url: [scheme: "http", host: "0.0.0.0", port: 8081], #DEPLOYMENT
+  url: [scheme: "http", host: "0.0.0.0", port: 80], #DEPLOYMENT
   # url: [scheme: "https", host: "mighty-escarpment-88517.herokuapp.com", port: 443],
   # force_ssl: [rewrite_on: [:x_forwarded_proto]],
   # force_ssl: [hsts: true],

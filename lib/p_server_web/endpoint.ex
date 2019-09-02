e0f5bf6216c48db68dbe8327574dd29647de2f0d
John Doe <jesusvelasquezoficial@gmail.com>
2019-09-02 10:16:13 -0400

    update

--- lib/p_server_web/endpoint.ex
+++ lib/p_server_web/endpoint.ex
@@ -43,7 +43,7 @@ defmodule PServerWeb.Endpoint do
     signing_salt: "/iB/chNg"
 
   # ADD el enchufe de la nueva dependencia
-  plug CORSPlug, origin: "http://10.0.1.14:8081"
+  plug CORSPlug, origin: "https://www.phoenixserver:8080"
   plug Plug.SSL, rewrite_on: [:x_forwarded_proto]
 
   plug PServerWeb.Router
